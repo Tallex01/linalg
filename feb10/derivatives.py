93a07df519cd802338032756c14485bef320ae6a
Alex Goodman <79115163+Tallex01@users.noreply.github.com>
2026-02-17 22:55:19 +0000

    comitt2

---
+++ feb10/derivatives.py
@@ -0,0 +1,10 @@
+import torch
+
+x = torch.tensor(2.0, requires_grad = True)
+y = torch.tensor(3.0, requires_grad = True)
+
+f = (3*y**2*x**2 + x**3*y**2) / (4*x**2*y**2 + 3*x**2*y**2 + 3)
+f.backward()
+print(x.grad)
+print(y.grad)
+#x.grad.zero_()
